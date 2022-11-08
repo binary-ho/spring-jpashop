b0a1e4518817f676775e0524973adcf9356f5e23
Jinho <dfghcvb11@naver.com>
2022-11-08 17:06:33 +0900

    feat: develop entity class - lec 2 done.

--- src/main/java/jpabook/jpashop/domain/Delivery.java
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -1,15 +1,21 @@
 package jpabook.jpashop.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 
+import static javax.persistence.FetchType.LAZY;
+
 @Entity
+@Getter @Setter
 public class Delivery {
 
     @Id @GeneratedValue
     @Column(name = "delivery_id")
     private Long Id;
 
-    @OneToOne(mappedBy = "delivery")
+    @OneToOne(mappedBy = "delivery", fetch = LAZY)
     private Order order;
 
     @Embedded
