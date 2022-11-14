bd0872bf7ec2d98b70b4cc5c9c4cadd64f79b872
Jinho <dfghcvb11@naver.com>
2022-11-14 23:16:00 +0900

    feat: clone the JPA 2 lecture 3 to 3.5 - collection query basic

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -33,6 +33,27 @@ public class OrderRepository {
             .getResultList();
     }
 
+    // XToOne 관계는 아무리 fetch join 해도 문제가 없어
+    public List<Order> findAllWithMemberDelivery(int offset, int limit) {
+        return em.createQuery(
+                "select o from Order as o join fetch o.member m join fetch o.delivery d", Order.class)
+            .setFirstResult(offset)
+            .setMaxResults(limit)
+            .getResultList();
+    }
+
+    public List<Order> findAllWithItem() {
+        return em.createQuery(
+                "select distinct o from Order o"
+                    + " join fetch o.member m"
+                    + " join fetch o.delivery d"
+                    + " join fetch o.orderItems oi"
+                    + " join fetch oi.item i", Order.class)
+            .getResultList();
+    }
+
+
+
 //    public List<Order> findAll(OrderSearch orderSearch) {
 //        return em.createQuery("select o from Order as o join o.member as m " +
 //                        "where o.status = :status " +
